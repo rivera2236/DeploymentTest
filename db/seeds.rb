dad78ee4646cc07800ae745ab91b12e190664c64
jorge rivera <rivera2236@gmail.com>
2019-10-23 19:38:24 -0400

    commit after installin postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+10.times do 
+	User.create(name: Faker::Name.unique.name)
+end
+
+# develope enviroment vs production enviroment.
+# sqlite3
+# pg 
+
